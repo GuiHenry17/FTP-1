c7808e991abc64f68a93d9adf6075f3548477523
Guilherme Henrique Santos Pereira <guiper@alunos.utfpr.edu.br>
2026-06-08 03:39:53 -0300

    ---

--- lista8-funcvetores_guilhermeHenrique/funcoes.js
+++ lista8-funcvetores_guilhermeHenrique/funcoes.js
@@ -1,3 +1,5 @@
+// BÁSICO
+
 function preencherVetor(nEntradas) {
     let entradas = [], i;
     for (i = 0; i < nEntradas; i++) {
@@ -136,3 +138,154 @@ function encontrarMenor(vetor = []) {
     return menor
 }
 
+function verficarAprovacao(vetor = [], criterio) {
+    let i
+    for (i = 0; i < vetor.length; i++) {
+        if (vetor[i] >= criterio) {
+            document.write(`${i + 1}º nota: ${vetor[i]} - Aprovado <br>`)
+        } else {
+            document.write(`${i + 1}º nota: ${vetor[i]} - Reprovado <br>`)
+        }
+    }
+}
+
+// INTERMEDIÁRIO
+
+function encontrarIndiceDoMaiorValor(vetor = []) {
+    let i, maiorValor, indiceDoMaiorValor;
+    for (i = 0; i < vetor.length; i++) {
+        if (i == 0) {
+            maiorValor = vetor[i]
+            indiceDoMaiorValor = i
+        }
+        if (vetor[i] > maiorValor) {
+            maiorValor = vetor[i]
+            indiceDoMaiorValor = i
+        }
+    }
+
+    return indiceDoMaiorValor
+}
+
+function verificarExistenciaDeValor(vetor = [], numeroProcurado) {
+    let i, encontrado;
+    for (i = 0; i < vetor.length; i++) {
+        if (vetor[i] == numeroProcurado) {
+            encontrado = true
+            break;
+        }
+        else {
+            encontrado = false
+        }
+    }
+
+    if (encontrado) {
+        document.write(`<hr>O número ${numeroProcurado} está presente no vetor.`)
+    } else {
+        document.write(`<hr>O número ${numeroProcurado} NÃO está presente no vetor.`)
+    }
+}
+
+function contarNotasAcimaDaMedia(vetor = [], vetorOpcional = []) {
+    let media = calcularMedia(vetor), contador = 0, i, contadorVetorOpcional = 0;
+    for (i = 0; i < vetor.length; i++) {
+        if (vetor[i] > media) {
+            contador++
+            if (vetorOpcional) {
+                vetorOpcional[contadorVetorOpcional] = vetor[i]
+                contadorVetorOpcional++
+            }
+        }
+    }
+
+    return contador
+}
+
+function contarMultiplosDe3(vetor = [], vetorMultiplosDe3 = []) {
+    let contador = 0, i, contadorMultiplosDe3 = 0;
+    for (i = 0; i < vetor.length; i++) {
+        if (vetor[i] % 3 == 0) {
+            contador++
+            if (vetorMultiplosDe3) {
+                vetorMultiplosDe3[contadorMultiplosDe3] = vetor[i]
+                contadorMultiplosDe3++
+            }
+        }
+    }
+    return contador
+}
+
+function calcularSomaDosPares(vetor = []) {
+    let soma = 0, i;
+    for (i = 0; i < vetor.length; i++) {
+        if (vetor[i] % 2 == 0) {
+            soma += vetor[i]
+        }
+    }
+    return soma
+}
+
+function contarNumerosIgualA(vetor = [], criterio, vetorOpcional = []) {
+    let contador = 0, i, iVOpcional = 0;
+    for (i = 0; i < vetor.length; i++) {
+        if (vetor[i] == criterio) {
+            contador++
+            if (vetorOpcional) {
+                vetorOpcional[iVOpcional] = vetor[i]
+                iVOpcional++
+            }
+        }
+    }
+    return contador
+}
+
+function contarNumerosEntre(vetor = [], limiteInferior, limiteSuperior, vetorOpcional = []) {
+    let contador = 0, i, iVOpcional = 0; 
+    for (i = 0; i < vetor.length; i++) {
+        if (vetor[i] >= limiteInferior && vetor[i] <= limiteSuperior) {
+            contador++
+            if (vetorOpcional) {
+                vetorOpcional[iVOpcional] = vetor[i]
+                iVOpcional++
+            }
+        }
+    }
+    return contador
+}
+
+function encontrarIndiceDoMenorValor(vetor = []) {
+    let i, menorValor, indiceDoMenorValor;
+    for (i = 0; i < vetor.length; i++) {
+        if (i == 0) {
+            menorValor = vetor[i]
+            indiceDoMenorValor = i
+        }
+        if (vetor[i] < menorValor) {
+            menorValor = vetor[i]
+            indiceDoMenorValor = i
+        }
+    }
+
+    return indiceDoMenorValor
+}
+
+function calcularDiferenca(vetor = []) {
+    let maiorValor = encontrarMaior(vetor), menorValor = encontrarMenor(vetor)
+    return maiorValor - menorValor
+}
+
+// LANCHONETE
+
+function contarNumerosMaiorOuIgualA(vetor = [], criterio, vetorOpcional = []) {
+    let contador = 0, i, iVOpcional = 0;
+    for (i = 0; i < vetor.length; i++) {
+        if (vetor[i] >= criterio) {
+            contador++
+            if (vetorOpcional) {
+                vetorOpcional[iVOpcional] = vetor[i]
+                iVOpcional++
+            }
+        }
+    }
+    return contador
+}
